803d1b78b6f173527adb7d93bec9e46be770172b
HoeYoung Lim <dlaghldud@tukorea.ac.kr>
2025-07-31 02:21:58 +0900

    test commit

---
+++ src/main/java/com/example/demo_mp3_codec_chanage/DemoMp3CodecChanageApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_mp3_codec_chanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMp3CodecChanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMp3CodecChanageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo_mp3_codec_chanage/DemoMp3CodecChanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_mp3_codec_chanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMp3CodecChanageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
