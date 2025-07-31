23a240123e284d9b58d545f9a0de192ec30793d8
HoeYoung Lim <dlaghldud@tukorea.ac.kr>
2025-08-01 02:13:29 +0900

    run test

--- src/main/java/com/example/demo_mp3_codec_chanage/DemoMp3CodecChanageApplication.java
+++ src/main/java/com/example/demo_mp3_codec_chanage/DemoMp3CodecChanageApplication.java
@@ -1,9 +1,28 @@
+/*
 package com.example.demo_mp3_codec_chanage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+public class DemoMp3CodecChanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMp3CodecChanageApplication.class, args);
+    }
+
+}
+*/
+package com.example.demo_mp3_codec_chanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+
+
 public class DemoMp3CodecChanageApplication {
 
     public static void main(String[] args) {
